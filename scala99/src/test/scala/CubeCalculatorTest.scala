22853a5146b85f0c9c834c2db78116d7c22b116c
SnailDux <przemekk97@gmail.com>
2023-08-13 18:08:44 +0200

    added some testing

--- scala99/src/test/scala/CubeCalculatorTest.scala
+++ scala99/src/test/scala/CubeCalculatorTest.scala
@@ -1,5 +1,19 @@
+import task1.functions.last1
+
 class CubeCalculatorTest extends org.scalatest.funsuite.AnyFunSuite {
-  test("CubeCalculator.cube") {
-    assert(CubeCalculator.cube(3) === 27)
+
+  test("task1.functions.last1, should give last element of list"){
+    assert(last1(List(1,2,3)) === 3)
   }
+
+  test("task1.functions.last2, should give last element of list"){
+    assert(last1(List(1,2,3)) != 5)
+  }
+
+  test("task1.functions.last1, last of empty list should throw an error"){
+    assertThrows[NoSuchElementException]{
+      (last1(List()))
+    }
+  }
+
 }
